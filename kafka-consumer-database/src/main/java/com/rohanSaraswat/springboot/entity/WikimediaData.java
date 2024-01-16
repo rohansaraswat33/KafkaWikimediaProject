eff0e2fb794bd82341c540b201ed71bbdc638f3f
rohansaraswat33 <rohansaraswat33@gmail.com>
2024-01-16 13:20:15 +0530

    .

---
+++ kafka-consumer-database/src/main/java/com/rohanSaraswat/springboot/entity/WikimediaData.java
@@ -0,0 +1,18 @@
+package com.rohanSaraswat.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="wikimedia_recentchange")
+@Getter
+@Setter
+public class WikimediaData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    @Column(columnDefinition = "longtext")
+    private String wikiEventData;
+}
---
+++ src/main/java/com/rohanSaraswat/springbootwikimediaproject/SpringBootWikimediaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rohanSaraswat.springbootwikimediaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWikimediaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWikimediaProjectApplication.class, args);
+	}
+
+}
